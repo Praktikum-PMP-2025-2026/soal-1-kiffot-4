eea1f691a5d6c8c6e2ea933685a81c9e4cd94548
kiffot <cliffordteja@gmail.com>
2026-05-11 09:13:02 +0700

    feat add code

--- soal1.c
+++ soal1.c
@@ -1,5 +1,51 @@
 #include <stdio.h>
 
-int main(void) {
-    return 0;
+int main() {
+    int n=0;
+    scanf("%d",&n);
+    int adj[n][n];
+    for (int i =0; i <n; i++){
+        for (int j =0; j <n; j++){
+            scanf("%d", &adj[i][j]);
+        }
+    }
+    for (int i =0; i <n; i++){
+        for (int j =0; j <n; j++){
+            printf("%d", adj[i][j]);
+        }
+    }
+    int deg[n];
+    int max;
+    int maxidx;
+    int isolated[n];
+    for (int i =0; i <n; i++){
+        deg[i]=0;
+        isolated[i]=0;
+        for (int j =0; j <n; j++){
+            if (adj[i][j]==1){
+                deg[i]+=1;
+            }
+        }
+        if (deg[n]>max){
+            max = deg[n];
+            maxidx=i;
+        }
+
+        if (deg[i]==0){
+            isolated[i]=1;
+        }
+    }
+    
+    for (int i =0; i <n; i++){
+        printf("DEGREE %d %d\n", i, deg[i]);
+    }
+    printf("MAX_VERTEX %d\n", maxidx);
+    printf("ISOLATED");
+    for (int i =0; i <n; i++){
+        if (isolated[i]!=0){
+            printf(" %d", i);
+        }
+        
+    }
+
 }
